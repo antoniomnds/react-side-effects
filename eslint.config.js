a3fd9b3222a9e153d7250ab21b0bc7225eab6f54
Antonio Santos <antoniomndsantos@gmail.com>
2024-11-29 10:41:03 +0000

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    settings: {
+      react: {
+        version: "detect"
+      }
+    },
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    rules: {
+      "react/prop-types": "off",
+    }
+  },
+];
